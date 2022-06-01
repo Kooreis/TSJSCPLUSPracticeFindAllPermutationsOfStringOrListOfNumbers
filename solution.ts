414689dbc18914fa2c16de5d6f1b606c30208870
Kooreis <kooreis.official@gmail.com>
2022-06-01 08:53:34 -0500

    This is the first part of the function that handles the case when the input is a string. It generates all permutations of the string by picking each character and recursively generating permutations of the remaining characters.

--- solution.ts
+++ solution.ts
@@ -1,6 +1,3 @@
-Here is a TypeScript solution for finding all permutations of a string or list of numbers:
-
-```typescript
 function getPermutations(input: string | number[]): string[] | number[][] {
     if (typeof input === 'string') {
         if (input.length === 1) return [input];
@@ -13,21 +10,4 @@ function getPermutations(input: string | number[]): string[] | number[][] {
                 permutations.push(char + subPermutation);
         }
         return permutations;
-    } else {
-        if (input.length === 1) return [input];
-        const permutations: number[][] = [];
-        for (let i = 0; i < input.length; i++) {
-            const num = input[i];
-            const remainingNums = input.filter((_, index) => index !== i);
-            for (let subPermutation of getPermutations(remainingNums))
-                permutations.push([num].concat(subPermutation));
-        }
-        return permutations;
     }
-}
-
-console.log(getPermutations('abc'));
-console.log(getPermutations([1, 2, 3]));
-```
-
-This solution uses a recursive function `getPermutations` to generate all permutations of a string or a list of numbers. The function checks the type of the input and handles it accordingly. If the input is a string, it generates all permutations by picking each character and recursively generating permutations of the remaining characters. If the input is a list of numbers, it generates all permutations by picking each number and recursively generating permutations of the remaining numbers. The generated permutations are then printed to the console.
