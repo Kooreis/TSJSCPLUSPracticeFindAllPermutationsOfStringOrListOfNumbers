99f744d33d9d50f29960c67c5e0e5794495b1549
Kooreis <kooreis.official@gmail.com>
2022-06-02 08:53:34 -0500

    In this update, the permute function is defined. This function takes a string and two integers as parameters. It prints all the permutations of the string by swapping characters and recursively calling itself.

--- solution.cpp
+++ solution.cpp
@@ -1,3 +1,14 @@
-#include <iostream>
-#include <string>
-#include <algorithm>
+void permute(std::string str, int l, int r)
+{
+    if (l == r)
+        std::cout << str << std::endl;
+    else
+    {
+        for (int i = l; i <= r; i++)
+        {
+            std::swap(str[l], str[i]);
+            permute(str, l + 1, r);
+            std::swap(str[l], str[i]);
+        }
+    }
+}
